d5fbef2f5a2888724521420023586aaae8d339bb
hhimanshu <harit.himanshu@yahoo.in>
2021-02-22 10:08:28 -0800

    Added tests for /transactions API

--- jest.config.ts
+++ jest.config.ts
@@ -77,9 +77,7 @@ export default {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: [
-    "dist"
-  ],
+  modulePathIgnorePatterns: ['dist'],
 
   // Activates notifications for test results
   // notify: false,
