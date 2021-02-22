4123294f67221e7402a38ee65e83ece225f77608
hhimanshu <harit.himanshu@yahoo.in>
2021-02-22 12:04:07 -0800

    Making husky work

--- src/api/customers.ts
+++ src/api/customers.ts
@@ -4,9 +4,9 @@ import { Customer } from '../declaration';
 const router = express.Router();
 
 router.get('/newest', (req, res) => {
-  const c1: Customer = { first: 'Tony', last: 'Stark' }
-  const c2: Customer = { first: 'Joy', last: 'Pinilla' }
+  const c1: Customer = { first: 'Tony', last: 'Stark' };
+  const c2: Customer = { first: 'Joy', last: 'Pinilla' };
   res.send([c1, c2]);
-})
+});
 
 export default router;
